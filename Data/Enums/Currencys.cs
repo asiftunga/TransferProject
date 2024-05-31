898c38697065bfc7cbf307675e81e3dcc854f12b
asif tunga mubarek <asiftunga@hotmail.com>
2024-05-31 23:47:00 +0300

    payment methods ve currency enumlari eklendi

---
+++ Data/Enums/Currencys.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniApp1Api.Data.Enums;
+
+public enum Currencys
+{
+    Unknown = 0,
+    TRY = 1,
+    USD = 2,
+    TMT = 3,
+}
---
+++ Data/Enums/PaymentMethods.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniApp1Api.Data.Enums;
+
+public enum PaymentMethods
+{
+    Unknown = 0,
+    PhoneNumber = 1,
+    Remittence = 2,
+    Address = 3
+}
