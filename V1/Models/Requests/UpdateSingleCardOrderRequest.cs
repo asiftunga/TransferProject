c4f669adc91565fe4031cfc005d450c00fed18d3
asif tunga mubarek <asiftunga@hotmail.com>
2024-06-01 18:15:40 +0300

    SingleUseCardV1Controller.cs refactored

---
+++ V1/Models/Requests/UpdateSingleCardOrderRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniApp1Api.V1.Models.Requests;
+
+public class UpdateSingleCardOrderRequest
+{
+    public string? CardName { get; set; }
+
+    public string CardNumber { get; set; }
+
+    public DateTime CardDate { get; set; }
+
+    public string CVV { get; set; }
+}
